4940317b822f48282c27edef8acdf61d6f2862bf
aakash0456 <aakashsharma0456@gmail.com>
2026-07-19 11:44:53 -0500

    DSAMaster v1.2.0 - File changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.dsamasterio.github.aakash0456.dsamaster"
+        applicationId = "io.github.aakash0456.dsamaster"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
